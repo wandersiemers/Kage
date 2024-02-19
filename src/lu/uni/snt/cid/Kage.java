b5a747c237de87b08707898190e3288ac8164f8c
Wander <wandersiemers@me.com>
2024-02-19 14:38:53 +0100

    Cleanup: applying quickfixes and extracting duplicated code, removing commented out / unused code

--- src/lu/uni/snt/cid/Kage.java
+++ src/lu/uni/snt/cid/Kage.java
@@ -41,7 +41,7 @@ public class Kage
 
 	public static void mine(String apkPath, String androidJars)
 	{
-		//(1) Unzip Android APK and extract all additonally accessed DEXes
+		//(1) Unzip Android APK and extract all additionally accessed DEXes
 		Set<String> additionalDexes = new DexHunter(apkPath).hunt();
 		
 		//(2) Extracting the targeted Sdk version
@@ -77,10 +77,10 @@ public class Kage
 		System.out.println("Collected " + extractor.primaryAPIs.size() + " " + "Android APIs in the primary DEX file");
 		System.out.println("Collected " + extractor.secondaryAPIs.size() + " " + "Android APIs in the secondary DEX files");
 		
-		Set<APILife> problematicAPIs_forward = new HashSet<APILife>();
-		Set<APILife> protectedAPIs_forward = new HashSet<APILife>();
-		Set<APILife> problematicAPIs_backward = new HashSet<APILife>();
-		Set<APILife> protectedAPIs_backward = new HashSet<APILife>();
+		Set<APILife> problematicAPIs_forward = new HashSet<>();
+		Set<APILife> protectedAPIs_forward = new HashSet<>();
+		Set<APILife> problematicAPIs_backward = new HashSet<>();
+		Set<APILife> protectedAPIs_backward = new HashSet<>();
 		
 		for (String method : extractor.usedAndroidAPIs)
 		{
@@ -131,100 +131,49 @@ public class Kage
 		for (APILife lifetime : protectedAPIs_forward)
 		{
 			System.out.println("\n==>Protected_Forward" + lifetime);
-			System.out.println(extractor.api2callers.get(lifetime.getSignature()));
-			for (String methodSig : extractor.api2callers.get(lifetime.getSignature()))
-			{
-				boolean isLibraryMethod = AndroidLibraries.isAndroidLibrary(new MethodSignature(methodSig).getCls());
-				if (isLibraryMethod)
-				{
-					System.out.println("--Library:True-->" + lifetime + "-->" + methodSig);
-				}
-				else
-				{
-					System.out.println("--Library:False-->" + lifetime + "-->" + methodSig);
-					System.out.println(ConditionalCallGraph.obtainCallStack(methodSig));
-				}
-			}
+			printMethod(extractor, lifetime);
 		}
 		
 		for (APILife lifetime : problematicAPIs_forward)
 		{
 			System.out.println("\n==>Problematic_Forward" + lifetime);
-			System.out.println(extractor.api2callers.get(lifetime.getSignature()));
-			for (String methodSig : extractor.api2callers.get(lifetime.getSignature()))
-			{
-				boolean isLibraryMethod = AndroidLibraries.isAndroidLibrary(new MethodSignature(methodSig).getCls());
-				if (isLibraryMethod)
-				{
-					System.out.println("--Library:True-->" + lifetime + "-->" + methodSig);
-				}
-				else
-				{
-					System.out.println("--Library:False-->" + lifetime + "-->" + methodSig);
-					System.out.println(ConditionalCallGraph.obtainCallStack(methodSig));
-				}
-			}
+			printMethod(extractor, lifetime);
 		}
 		
 		
 		for (APILife lifetime : protectedAPIs_backward)
 		{
 			System.out.println("\n==>Protected_Backward" + lifetime);
-			System.out.println(extractor.api2callers.get(lifetime.getSignature()));
-			for (String methodSig : extractor.api2callers.get(lifetime.getSignature()))
-			{
-				boolean isLibraryMethod = AndroidLibraries.isAndroidLibrary(new MethodSignature(methodSig).getCls());
-				if (isLibraryMethod)
-				{
-					System.out.println("--Library:True-->" + lifetime + "-->" + methodSig);
-				}
-				else
-				{
-					System.out.println("--Library:False-->" + lifetime + "-->" + methodSig);
-					System.out.println(ConditionalCallGraph.obtainCallStack(methodSig));
-				}
-			}
+			printMethod(extractor, lifetime);
 		}
 		
 		for (APILife lifetime : problematicAPIs_backward)
 		{
 			System.out.println("\n==>Problematic_Backward" + lifetime);
-			System.out.println(extractor.api2callers.get(lifetime.getSignature()));
-			for (String method : extractor.api2callers.get(lifetime.getSignature()))
+			printMethod(extractor, lifetime);
+		}
+	}
+
+	private static void printMethod(APIExtractor extractor, APILife lifetime) {
+		System.out.println(extractor.api2callers.get(lifetime.getSignature()));
+		for (String methodSig : extractor.api2callers.get(lifetime.getSignature()))
+		{
+			boolean isLibraryMethod = AndroidLibraries.isAndroidLibrary(new MethodSignature(methodSig).getCls());
+			if (isLibraryMethod)
 			{
-				boolean isLibraryMethod = AndroidLibraries.isAndroidLibrary(new MethodSignature(method).getCls());
-				if (isLibraryMethod)
-				{
-					System.out.println("--Library:True-->" + lifetime + "-->" + method);
-				}
-				else
-				{
-					System.out.println("--Library:False-->" + lifetime + "-->" + method);
-					System.out.println(ConditionalCallGraph.obtainCallStack(method));
-				}	
+				System.out.println("--Library:True-->" + lifetime + "-->" + methodSig);
+			}
+			else
+			{
+				System.out.println("--Library:False-->" + lifetime + "-->" + methodSig);
+				System.out.println(ConditionalCallGraph.obtainCallStack(methodSig));
 			}
 		}
 	}
-	
-	public static String constraint(int min1, int max1, int min2, int max2)
-	{
-		int min = min1 > min2 ? min1 : min2;
-		int max = max1 > max2 ? max2 : max1;
-		
-		return min + "," + max;
-	}
-	
-	public static String constraint(int min1, int max1, String minMax)
-	{
-		int min2 = Integer.parseInt(minMax.split(",")[0]);
-		int max2 = Integer.parseInt(minMax.split(",")[1]);
-		
-		return constraint(min1, max1, min2, max2);
-	}
-	
+
 	public static int inferAPILevel(AndroidManifest manifest)
 	{
-		int apiLevel = -1;
+		int apiLevel;
 		if (-1 != manifest.getTargetSdkVersion())
 		{
 			apiLevel = manifest.getTargetSdkVersion();
@@ -253,56 +202,3 @@ public class Kage
 		}
 	}
 }
-
-
-
-//(4) Extracting the SDK version checks
-//Map<String, APISDKCheck> method2SDKChecks = AndroidSDKVersionChecker.scan(apkPath, androidJars);
-
-/*
-String constraint4Primary = Integer.MIN_VALUE + "," + Integer.MAX_VALUE;
-String constraint4Primary2 = Integer.MIN_VALUE + "," + Integer.MAX_VALUE;
-StringBuilder content = new StringBuilder();
-for (String method : extractor.primaryAPIs)
-{
-	content.append(method + "\n");
-	
-	APILife lifetime = AndroidAPILifeModel.getInstance().getLifetime(method);
-	constraint4Primary = constraint(lifetime.getMinAPILevel(), lifetime.getMaxAPILevel(), constraint4Primary);
-	
-	APILife lifetime2 = AndroidAPILifeModel.getInstance().getLifetime(method, method2SDKChecks);
-	constraint4Primary2 = constraint(lifetime2.getMinAPILevel(), lifetime2.getMaxAPILevel(), constraint4Primary2);
-}
-CommonUtils.writeResultToFile("mining4u_primary_apis_" + apkName + ".txt", content.toString());
-
-Set<String> allAPIs = new HashSet<String>();
-allAPIs.addAll(extractor.primaryAPIs);
-allAPIs.addAll(extractor.secondaryAPIs);
-
-String constraint4Secondary = Integer.MIN_VALUE + "," + Integer.MAX_VALUE;
-String constraint4Secondary2 = Integer.MIN_VALUE + "," + Integer.MAX_VALUE;
-
-content = new StringBuilder();
-for (String method : allAPIs)
-{
-	content.append(method + "\n");
-	
-	APILife lifetime = AndroidAPILifeModel.getInstance().getLifetime(method);
-	constraint4Secondary = constraint(lifetime.getMinAPILevel(), lifetime.getMaxAPILevel(), constraint4Secondary);
-	
-	APILife lifetime2 = AndroidAPILifeModel.getInstance().getLifetime(method, method2SDKChecks);
-	constraint4Secondary2 = constraint(lifetime2.getMinAPILevel(), lifetime2.getMaxAPILevel(), constraint4Secondary2);
-	
-	System.out.println("==>" + method + ":[" + lifetime2.getMinAPILevel() + "," + lifetime2.getMaxAPILevel() + "]");
-}
-CommonUtils.writeResultToFile("mining4u_secondary_apis_" + apkName + ".txt", content.toString());
-
-System.out.println("Declared Min Sdk version is: " + manifest.getMinSdkVersion());
-System.out.println("Declared Target Sdk version is: " + manifest.getTargetSdkVersion());
-System.out.println("Declared Max Sdk version is: " + manifest.getMaxSdkVersion());
-
-System.out.println("Expected Sdk versions are (without DCL): " + constraint4Primary);
-System.out.println("Expected Sdk versions are (without DCL, With SDK Check): " + constraint4Primary2);
-System.out.println("Expected Sdk versions are (with DCL): " + constraint4Secondary);
-System.out.println("Expected Sdk versions are (with DCL, With SDK Check): " + constraint4Secondary2);
-*/
