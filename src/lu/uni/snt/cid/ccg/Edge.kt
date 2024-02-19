39df4f39d66db4262b5b903d6457d30e068e0656
Wander <wandersiemers@me.com>
2024-02-19 15:47:59 +0100

    Converted Edge to Kotlin

---
+++ src/lu/uni/snt/cid/ccg/Edge.kt
@@ -0,0 +1,15 @@
+package lu.uni.snt.cid.ccg
+
+class Edge {
+    @JvmField
+	var srcSig: String = ""
+    @JvmField
+	var tgtSig: String = ""
+
+    @JvmField
+	var conditions: Set<String> = HashSet()
+
+    override fun toString(): String {
+        return "$conditions:$srcSig-->$tgtSig"
+    }
+}
