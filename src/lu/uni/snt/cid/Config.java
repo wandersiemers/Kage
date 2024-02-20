211a437c39126a3f35b759235b440a0332a42555
Wander <wandersiemers@me.com>
2024-02-20 12:00:54 +0100

    Update latest and default API level

--- src/lu/uni/snt/cid/Config.java
+++ src/lu/uni/snt/cid/Config.java
@@ -2,8 +2,8 @@ package lu.uni.snt.cid;
 
 public class Config 
 {
-	public static final int LATEST_API_LEVEL = 25;
-	public static final int DEFAULT_API_LEVEL = 19;
+	public static final int LATEST_API_LEVEL = 34;
+	public static final int DEFAULT_API_LEVEL = 25;
 	
 	public static final String FIELD_VERSION_SDK_INT = "<android.os.Build$VERSION: int SDK_INT>";
 	public static final String FIELD_VERSION_SDK = "<android.os.Build$VERSION: java.lang.String SDK>";
