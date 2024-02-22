1545ca5aec3ee4b2cfd3b1663f69186f14816b1b
Wander <wandersiemers@me.com>
2024-02-22 13:31:29 +0100

    Extract methods for clarity

--- src/lu/uni/snt/cid/ccg/AndroidSDKVersionChecker.kt
+++ src/lu/uni/snt/cid/ccg/AndroidSDKVersionChecker.kt
@@ -41,58 +41,24 @@ object AndroidSDKVersionChecker {
 
         while (true) {
             if (stmt is AssignStmt) {
-                val leftOp = stmt.leftOp
-
-                if (stmt.toString().contains(Config.FIELD_VERSION_SDK_INT)) {
-                    sdkIntValues.add(leftOp)
-                } else {
-                    //Remove killed references
-                    sdkIntValues.remove(leftOp)
-                }
+                handleAssignStmt(stmt, sdkIntValues)
+                handleAnimationAssignStmt(stmt, HashSet())
             }
 
             if (stmt.containsInvokeExpr()) {
-                val edge = getEdge(
-                    b.method.signature,
-                    stmt.invokeExpr.method.signature
-                )
-                edge!!.conditions.add(conditions.toString())
+                if (handleInvokeExpr(b, stmt, conditions)) continue
+                if (handleInvokeAnimatorsEnabledExpr(b, stmt, conditions)) continue
 
-                addEdge(edge)
-
-                if (stmt.invokeExpr is InterfaceInvokeExpr) {
-                    val sootMethod = stmt.invokeExpr.method
-
-                    if (sootMethod.declaration.contains("private")) {
-                        // If the method is declared as private, then it cannot be extended by the subclasses.
-                        continue
-                    }
-
-                    val sootClass = sootMethod.declaringClass
-                    val subClasses = SootUtils.getAllSubClasses(sootClass)
-
-                    for (subClass in subClasses) {
-                        val e = getEdge(
-                            edge.sourceSig,
-                            edge.targetSig.replace(sootClass.name + ":", subClass.name + ":")
-                        )
-
-                        e!!.conditions.addAll(edge.conditions)
-                        addEdge(e)
-                    }
-                }
             }
 
             if (stmt is IfStmt) {
-                for (vb in stmt.condition.useBoxes) {
-                    if (sdkIntValues.contains(vb.value)) {
-                        sdkChecker = true
-                        break
-                    }
-                }
+                sdkChecker = handleIfStmtAnimations(stmt)
+                sdkChecker = handleIfStmt(stmt, sdkIntValues, sdkChecker)
+
             }
 
             succUnits = graph.getSuccsOf(stmt)
+
             if (succUnits.size == 1) {
                 stmt = succUnits[0] as Stmt
 
@@ -134,5 +100,132 @@ object AndroidSDKVersionChecker {
                 traverse(b, graph, u, sdkIntValues, conditions, visitedUnits)
             }
         }
+    }
+
+    private fun handleAssignStmt(stmt: AssignStmt, sdkIntValues: MutableSet<Value?>) {
+        val leftOp = stmt.leftOp
+
+        if (stmt.toString().contains(Config.FIELD_VERSION_SDK_INT)) {
+            sdkIntValues.add(leftOp)
+        } else {
+            //Remove killed references
+            sdkIntValues.remove(leftOp)
+        }
+    }
+
+    private fun handleAnimationAssignStmt(stmt: AssignStmt, animationValues: MutableSet<Value?>) {
+        val leftOp = stmt.leftOp
+
+        if (stmt.toString().contains("areAnimatorsEnabled")) {
+            println("found animators enabled check in full statement $stmt")
+            animationValues.add(leftOp)
+        } else {
+            //Remove killed references
+            animationValues.remove(leftOp)
+        }
+    }
+
+    private fun handleIfStmt(
+        stmt: IfStmt,
+        sdkIntValues: MutableSet<Value?>,
+        sdkChecker: Boolean
+    ): Boolean {
+        var sdkChecker1 = sdkChecker
+        for (vb in stmt.condition.useBoxes) {
+            if (sdkIntValues.contains(vb.value)) {
+                sdkChecker1 = true
+                break
+            }
+        }
+        return sdkChecker1
+    }
+
+    private fun handleIfStmtAnimations(
+        stmt: IfStmt,
+    ): Boolean {
+        if (stmt.toString().contains("areAnimatorsEnabled", true)) {
+            println("found animators enabled check in if stmt  ${stmt.condition}")
+            return true
+        }
+        return false
+    }
+
+
+
+    private fun handleInvokeExpr(
+        b: Body,
+        stmt: Stmt,
+        conditions: Set<String?>
+    ): Boolean {
+        val edge = getEdge(
+            b.method.signature,
+            stmt.invokeExpr.method.signature
+        )
+        edge!!.conditions.add(conditions.toString())
+
+        addEdge(edge)
+
+        if (stmt.invokeExpr is InterfaceInvokeExpr) {
+            val sootMethod = stmt.invokeExpr.method
+
+            if (sootMethod.declaration.contains("private")) {
+                // If the method is declared as private, then it cannot be extended by the subclasses.
+                return true
+            }
+
+            val sootClass = sootMethod.declaringClass
+            val subClasses = SootUtils.getAllSubClasses(sootClass)
+
+            for (subClass in subClasses) {
+                val e = getEdge(
+                    edge.sourceSig,
+                    edge.targetSig.replace(sootClass.name + ":", subClass.name + ":")
+                )
+
+                e!!.conditions.addAll(edge.conditions)
+                addEdge(e)
+            }
+        }
+        return false
+    }
+
+    private fun handleInvokeAnimatorsEnabledExpr(body: Body, stmt: Stmt, conditions: Set<String?>): Boolean {
+        val edge = getEdge(
+            body.method.signature,
+            stmt.invokeExpr.method.signature
+        )
+        edge!!.conditions.add(conditions.toString())
+
+        addEdge(edge)
+
+        if (stmt.invokeExpr.method.signature.contains("encrypted", true)) {
+            println("invoked encrypted check, sig ${stmt.invokeExpr.method.signature}")
+        }
+
+
+        if (stmt.invokeExpr is InterfaceInvokeExpr) {
+            val sootMethod = stmt.invokeExpr.method
+
+            if (sootMethod.declaration.contains("private")) {
+                // If the method is declared as private, then it cannot be extended by the subclasses.
+                println("private method ${sootMethod.declaration}")
+                return true
+            }
+
+            val sootClass = sootMethod.declaringClass
+            val subClasses = SootUtils.getAllSubClasses(sootClass)
+
+            for (subClass in subClasses) {
+                val e = getEdge(
+                    edge.sourceSig,
+                    edge.targetSig.replace(sootClass.name + ":", subClass.name + ":")
+                )
+
+                e!!.conditions.addAll(edge.conditions)
+                addEdge(e)
+            }
+        }
+
+        return false
     }
 }
